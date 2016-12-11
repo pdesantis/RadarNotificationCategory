0c431149ad47238675204b5dbd83678c0e1a95b0
Pat DeSantis <pdesantis3@gmail.com>
2016-12-11 10:46:50 -0800

    Initial Commit

--- NotificationCategory/ViewController.swift
+++ NotificationCategory/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        reload()
+    }
+    
+    private func reload() {
+        // Disable the button until we know if the
+        // categories are registered or not
+        registerButton.isEnabled = false
+        label.text = "Loading..."
+        
+        UNUserNotificationCenter.current().getNotificationCategories { (categories) in
+            DispatchQueue.main.async {
+                if categories.isEmpty {
+                    self.label.text = "No categories registered"
+                } else {
+                    self.label.text = "\(categories.count) categories are registered"
+                }
+                self.registerButton.isEnabled = true
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func registerNotifications(_ sender: Any) {
+        let action = UNNotificationAction(identifier: "cool-action", title: "💸", options: [])
+        let category = UNNotificationCategory(identifier: "cool-category",
+                                              actions: [action],
+                                              intentIdentifiers: [],
+                                              options: [])
+        let categories = Set([category])
+        UNUserNotificationCenter.current().setNotificationCategories(categories)
+        reload()
     }
-
 
 }
 
